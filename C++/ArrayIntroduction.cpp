40556720cc9b38eb9df905ed92bc093c584af060
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-12-09 04:37:35 +0600

    Create ArrayIntroduction.cpp

---
+++ C++/ArrayIntroduction.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Array(int n,int arr[])
+{
+    for(int i=0,j=n-1;i<n/2;i++,j--)
+    {
+
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+
+
+    }
+    for(int i=0;i<n;i++)
+    {
+       cout<< arr[i]<<" ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {
+      cin>>arr[i];
+  }
+
+ Array(n,arr);
+}
