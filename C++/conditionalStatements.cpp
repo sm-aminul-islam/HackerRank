8f2d5c3f42d8cedd10e89083171137ad899377b5
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-12-09 02:57:21 +0600

    Create conditionalStatements.cpp

---
+++ C++/conditionalStatements.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ long long n;
+ cin>>n;
+ if(n>=1 and n<=9)
+ {
+     if(n==1)
+     {
+         cout<<"one"<<"\n";
+     }
+      if(n==2)
+     {
+         cout<<"two"<<"\n";
+     }
+      if(n==3)
+     {
+         cout<<"three"<<"\n";
+     }
+      if(n==4)
+     {
+         cout<<"four"<<"\n";
+     }
+      if(n==5)
+     {
+         cout<<"five"<<"\n";
+     }
+      if(n==6)
+     {
+         cout<<"six"<<"\n";
+     }
+      if(n==7)
+     {
+         cout<<"seven"<<"\n";
+     }
+      if(n==8)
+     {
+         cout<<"eight"<<"\n";
+     }
+      if(n==9)
+     {
+         cout<<"nine"<<"\n";
+     }
+ }
+ else{
+    cout<<"Greater than 9"<<"\n";
+ }
+
+    return 0;
+}
