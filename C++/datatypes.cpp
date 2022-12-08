925f37624aec6171523c96759b5de80c1996834c
S.M.Aminul Islam((Robin) <71812324+sm-aminul-islam@users.noreply.github.com>
2022-12-09 02:45:21 +0600

    Create datatypes.cpp

---
+++ C++/datatypes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n;  long long ll;char ch;  float f;double d;
+
+
+ cin>>n>>ll>>ch>>f>>d;
+    cout<<n<<"\n";
+       cout<<ll<<"\n";
+      cout<<ch<<"\n";
+   cout<<fixed<<setprecision(3)<<f<<"\n";
+   cout<<fixed<<setprecision(9)<<d<<"\n";
+
+
+
+    return 0;
+}
